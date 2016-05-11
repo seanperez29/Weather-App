c61ad125f7d7fa955823aac49b0e6b66b174946e
Sean Perez <sean.perez29@gmail.com>
2016-05-10 21:19:06 -0700

    Initial Commit

---
+++ Weather-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by Sean Perez on 5/10/16.
+//  Copyright © 2016 Sean Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
